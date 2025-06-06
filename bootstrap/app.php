e247690e84fca4c056a6a20e01c5e000370fd415
Shift <shift@laravelshift.com>
2025-06-06 14:56:32 +0000

    Re-register HTTP middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,22 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->redirectGuestsTo(fn () => route('login'));
+        $middleware->redirectUsersTo(RouteServiceProvider::HOME);
+
+        $middleware->throttleApi();
+
+        $middleware->alias([
+            'AdminLogin' => \App\Http\Middleware\AdminLogin::class,
+            'admin' => \App\Http\Middleware\Admin::class,
+            'check_if_super' => \App\Http\Middleware\CheckIfSuper::class,
+            'check_permission' => \App\Http\Middleware\CheckPermission::class,
+            'localeCookieRedirect' => \Mcamara\LaravelLocalization\Middleware\LocaleCookieRedirect::class,
+            'localeSessionRedirect' => \Mcamara\LaravelLocalization\Middleware\LocaleSessionRedirect::class,
+            'localeViewPath' => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationViewPath::class,
+            'localizationRedirect' => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationRedirectFilter::class,
+            'localize' => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationRoutes::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
